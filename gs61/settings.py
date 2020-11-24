7cee3c14152e0da84a671bf50d038d3f929c7942
Vijendra Khandelwal <52828885+vijjuk@users.noreply.github.com>
2020-11-24 15:05:53 +0530

    Update settings.py

--- gs61/settings.py
+++ gs61/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'achrb-pjh^mjh&qeek(=(i^3_r4lha4ap)%81-3&#(5k(rdknm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','']
 
 
 # Application definition
