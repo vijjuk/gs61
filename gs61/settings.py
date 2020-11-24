ff92e5d221de5460c78841a144a4d23a5ea126c7
Vijendra Khandelwal <52828885+vijjuk@users.noreply.github.com>
2020-11-24 15:12:01 +0530

    Update settings.py

--- gs61/settings.py
+++ gs61/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'achrb-pjh^mjh&qeek(=(i^3_r4lha4ap)%81-3&#(5k(rdknm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','']
+ALLOWED_HOSTS = ['127.0.0.1','image-wall1.herokuapp.com/']
 
 
 # Application definition
